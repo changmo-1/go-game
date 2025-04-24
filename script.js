04c281e492c19150ef3d8ab99330277cbd4b9d58
changmo-1 <changmoyu9@gmail.com>
2025-04-24 23:05:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function startGame() {
-  document.getElementById("start-screen").style.display = "none";
-  document.getElementById("controls").style.display = "block";
+  document.getElementById('start-screen').style.display = 'none';
+  document.getElementById('game-ui').style.display = 'block';
 }
+
 function moveLeft() {
   alert("왼쪽 이동!");
 }
+
 function moveRight() {
   alert("오른쪽 이동!");
 }
+
 function jump() {
   alert("점프!");
 }
