b120ca3234f874cfb6389f17076888ca50807a3d
changmo-1 <changmoyu9@gmail.com>
2025-04-24 23:14:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-window.onload = function () {
-  document.getElementById("startBtn").addEventListener("click", startGame);
-};
-
 function startGame() {
-  document.getElementById("start-screen").style.display = "none";
-  document.getElementById("game-ui").style.display = "block";
+  document.getElementById("message").style.display = "none";
+  document.querySelector("button").style.display = "none";
+  document.getElementById("stickman").classList.remove("hidden");
+  document.getElementById("controls").classList.remove("hidden");
 }
 
 function moveLeft() {
   alert("왼쪽으로 이동!");
 }
+
 function moveRight() {
   alert("오른쪽으로 이동!");
 }
+
 function jump() {
   alert("점프!");
 }
