7d140530959a38f36b057febe91c3f0b8e2b32b2
changmo-1 <changmoyu9@gmail.com>
2025-04-24 23:03:11 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-window.onload = () => {
-  document.getElementById("game").style.display = "block";
-  document.getElementById("controls").style.display = "none";
-};
-
 function startGame() {
-  document.getElementById("game").style.display = "none";
-  document.getElementById("controls").style.display = "flex";
-  // 여기에 게임 로직 시작 코드 추가 가능
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("controls").style.display = "block";
 }
-
 function moveLeft() {
-  alert("왼쪽으로 이동!");
+  alert("왼쪽 이동!");
 }
-
 function moveRight() {
-  alert("오른쪽으로 이동!");
+  alert("오른쪽 이동!");
 }
-
 function jump() {
   alert("점프!");
 }
